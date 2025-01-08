5d2f43dbbf5158ec6bcbca738f236fe088f06aeb
MD TAMIMUL ISLAM <tamimulislan@gmail.com>
2025-01-08 19:39:36 +0600

    new code

--- practice/dsu.cpp
+++ practice/dsu.cpp
@@ -15,35 +15,36 @@ public:
         this->n = n;
         parent.resize(n + 1);
         rank.resize(n + 1, 0);
-        for (int i = 0; i< n;i++)
+        for (int i = 0; i < n; i++)
             parent[i] = i;
     }
 
     int find(int x)
     {
-        if(parent[x] == x)
+        if (parent[x] == x)
             return x;
         return parent[x] = find(parent[x]);
     }
 
-    void Union(int a , int b)
+    bool Union(int a, int b)
     {
         int parA = find(a);
         int parB = find(b);
 
-        if(parA == parB)
-            return;
-        if(rank[parA] > rank[parB])
+        if (parA == parB)
+            return false;
+        if (rank[parA] > rank[parB])
             parent[parB] = parA;
 
-        if(rank[parB] > rank[parA])
+        if (rank[parB] > rank[parA])
             parent[parA] = parB;
 
-        if(rank[parA] == rank[parB])
+        if (rank[parA] == rank[parB])
         {
             parent[parB] = parA;
             rank[parA]++;
         }
+        return true;
     }
 
     int total_set()
@@ -51,8 +52,20 @@ public:
         int ans = 0;
         for (int i = 0; i < n; i++)
         {
-            if(parent[i] == i)
+            if (parent[i] == i)
                 ans++;
+        }
+        return ans;
+    }
+
+    int kruskal(vector<pair<int , pair<int , int >>> &edges)
+    {
+        sort(edges.begin() , edges.end());
+        int ans = 0;
+        for(auto &x : edges)
+        {
+            if(Union(x.second.first , x.second.second))
+                ans += x.first;
         }
         return ans;
     }
@@ -61,10 +74,30 @@ public:
 int main()
 {
     Onii_chan;
-    Disjoint_set dsu(5);
-    dsu.Union(0, 1);
-    dsu.Union(2, 3);
-    dsu.Union(0, 4);
-    cout << dsu.total_set() << uwu;
+    int nodes, edge, a, b, w;
+    cin >> nodes >> edge;
+    Disjoint_set dsu(nodes);
+    vector<pair<int, pair<int, int>>> edges;
+    for (int i = 0; i < edge; i++)
+    {
+        cin >> a >> b >> w;
+        edges.push_back({w, {a, b}});
+    }
+    cout << dsu.kruskal(edges) << uwu;
     return 0;
 }
+
+/*
+
+6 9
+0 5 10
+5 4 20
+4 1 40
+1 0 30
+0 2 50
+2 1 30
+1 3 70
+3 2 10
+3 4 10
+
+*/
