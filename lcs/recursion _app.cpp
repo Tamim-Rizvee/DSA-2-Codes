ef0702b662f190e729366990be1cec8679e4553f
MD TAMIMUL ISLAM <tamimulislan@gmail.com>
2025-01-13 21:55:44 +0600

    new codes

--- lcs/recursion _app.cpp
+++ lcs/recursion _app.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //to fimd the length of the longest common subsequence
-int lcs(string s1 , string s2 )
+int lcs(string s1 , string s2)
 {
     int n = s1.size(), m = s2.size();
     if(n == 0 || m == 0)
@@ -13,8 +13,8 @@ int lcs(string s1 , string s2 )
     if (s1[n - 1]==s2[m - 1])
         return 1 + lcs(s1.substr(0 , n - 1), s2.substr(0 , m - 1));
     int ans1 = lcs(s1.substr(0 , n - 1), s2);
-    int ans2 = lcs(s1, s2.substr(0 , m - 1));
-    return max(ans1 , ans2);
+    int ans2 = lcs(s1, s2.substr(0, m - 1));
+    return max(ans1, ans2);
 }
 
 
